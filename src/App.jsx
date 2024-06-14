95fb528ecc254eb40a6b7fdabe89d3fda493f79b
2shruti2 <shruthesharma@gmail.com>
2024-06-14 23:51:50 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import Footer from "./ui/components/Footer";
 import Navbar from "./ui/components/Navbar";
-import BestMissions from "./ui/sections/BestMissions";
 import Hero from "./ui/sections/Hero";
-import Highlights from "./ui/sections/Highlights";
-import LatestNews from "./ui/sections/LatestNews";
-import Locations from "./ui/sections/Locations";
-import Portals from "./ui/sections/Portals";
-import RecentUpdates from "./ui/sections/RecentUpdates";
 
 function App() {
   return (
