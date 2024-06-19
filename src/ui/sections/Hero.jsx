305d5490361f9ebb1901f572a9d17e2bb16ee7fc
Prashant Hazariwal <prashantHazariwal999@gmail.com>
2024-06-19 16:37:33 +0530

    setting primary and secondary color

--- src/ui/sections/Hero.jsx
+++ src/ui/sections/Hero.jsx
@@ -1,6 +1,31 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>Hero
+      <button className="text-white px-2 rounded bg-red-500">click me</button>
+      <button className="text-white px-2 rounded bg-red-600">click me</button>
+      <button className="text-white px-2 rounded bg-red-700">click me</button>
+      <button className="text-white px-2 rounded bg-red-800">click me</button>
+      <button className="text-white px-2 rounded bg-red-900">click me</button>
+      <button className="text-white px-2 rounded bg-red-400">click me</button>
+      <button className="text-white px-2 rounded bg-red-300">click me</button>
+      <button className="text-white px-2 rounded bg-red-200">click me</button>
+      <button className="text-white px-2 rounded bg-red-100">click me</button> 
+      <button className="text-white px-2 rounded bg-[#FF1400]">click me</button> 
+      <br />
+      hero
+      <button className="text-white px-2 rounded bg-blue-500">click me</button>
+      <button className="text-white px-2 rounded bg-blue-600">click me</button>
+      <button className="text-white px-2 rounded bg-blue-700">click me</button>
+      <button className="text-white px-2 rounded bg-blue-800">click me</button>
+      <button className="text-white px-2 rounded bg-blue-900">click me</button>
+      <button className="text-white px-2 rounded bg-blue-400">click me</button>
+      <button className="text-white px-2 rounded bg-blue-300">click me</button>
+      <button className="text-white px-2 rounded bg-blue-200">click me</button>
+      <button className="text-white px-2 rounded bg-blue-100">click me</button>
+      <button className="text-white px-2 rounded bg-[#0E0E96]">click me</button>
+    </div>
+
+
   )
 }
 
