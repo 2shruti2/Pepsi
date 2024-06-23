bd77e36ea76c5cf6358469b4450cca67fe312a04
Firewonder <86379357+Firewonder@users.noreply.github.com>
2024-06-23 20:48:48 +0530

    Basic Interafce Created

--- src/ui/sections/OurCollaborations.jsx
+++ src/ui/sections/OurCollaborations.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { ArrowRightCircleIcon } from '@heroicons/react/16/solid' 
 
 const OurCollaborations = () => {
   return (
-    <div className='h-screen  bg-primary'>
-      OurCollaborations
+    <div className='h-[120vh] p-10'>
+      <div className='container bg-secondary h-screen border rounded-3xl'>
+       <div className="text">
+        <h1 className='text-8xl font-bold mt-10 mb-5 mx-10'>Pepsi Collabs</h1>
+        <h3 className='text-xl font-semibold mx-10'>Get Wild With Pepsi Wild Cherry.</h3>
+        <div className="box p-10">
+        <div className='box1 h-[65vh] bg-primary w-[120vh] border rounded-3xl relative'>
+        <button className='px-16 py-5 border rounded-full bg-white text-black absolute bottom-7 right-6 text-2xl flex gap-4 font-normal'>View <ArrowRightCircleIcon className='text-black size-8'/></button>
+        </div>       
+        </div>
+       </div>
+      </div>
       </div>
   )
 }
