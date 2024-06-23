2a2acdbb81a9b10c7dc06565c78728c7a54ad1f0
Firewonder <86379357+Firewonder@users.noreply.github.com>
2024-06-23 17:54:41 +0530

    Our Collaborations section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import Footer from "./ui/components/Footer";
 // import Navbar from "./ui/components/Navbar";
 // import Hero from "./ui/sections/Hero";
+import OurCollaborations from "./ui/sections/OurCollaborations";
 import OurProducts from "./ui/sections/OurProducts";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="page-wrapper">
       {/* <Navbar />
       <Hero /> */}
+      <OurCollaborations/>
       <OurProducts/>
       {/* <Footer/> */}
     </div>
---
+++ src/ui/sections/OurCollaborations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurCollaborations = () => {
+  return (
+    <div className='h-screen  bg-primary'>
+      OurCollaborations
+      </div>
+  )
+}
+
+export default OurCollaborations
