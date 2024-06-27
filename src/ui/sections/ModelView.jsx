fa771e9c83687c38f34eacb69a9475c4541d05b3
2shruti2 <shruthesharma@gmail.com>
2024-06-28 00:24:13 +0530

    prototype 2

--- src/ui/sections/ModelView.jsx
+++ src/ui/sections/ModelView.jsx
@@ -2,7 +2,7 @@ import Model from "../components/Model"
 
 const ModelView = () => {
   return (
-    <div className="section-padding bg-blue-500 w-full min-h-screen">
+    <div className="section-padding  w-full min-h-screen">
       <Model/>
     </div>
   )
