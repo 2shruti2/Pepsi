c036788ddf2cd5eb6e343d3238ddc65cf800cdcf
Prashant Hazariwal <prashantHazariwal999@gmail.com>
2024-06-30 23:56:31 +0530

    hero added loader fixed

--- src/ui/sections/Hero.jsx
+++ src/ui/sections/Hero.jsx
@@ -1,108 +1,52 @@
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
-import AnimatedLoadaer from "../components/Hero/AnimatedLoadaer"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+
+const Hero = ({ image1, image2, image3 }) => {
 
 
-const Hero = () => {
-  let tl = gsap.timeline();
-
-useGSAP(()=>{
-  tl.from(".circle" , {
-    width : 0,
-    delay : 0.8,
-    duration:0.9,
-    ease: "power2.inout"
-  });
-  tl.to(".title" ,{
-    opacity:0,
-    delay:0.5,
-    duration: 0.8,
-    ease:"expo.inOut"
+  useGSAP(()=>{
+    gsap.from(".MHstarting",{
+      x:-10 ,
+      opacity:0,
+      delay:1,
+      duration:1
+    })
   })
-  tl.to(".divTitle" ,{
-    x:100,
-    delay:0.5,
-    duration: 0.8,
-    ease : "sine.inOut"
-  })
-  tl.to(".circle" , {
-    width : 0,
-    height: 0,
-    borderRadius : 0,
-    delay : -0.2,
-    duration:0.5,
-    ease:"easeinout"
-  });
-  tl.to(".circle2" , {
-    opacity:1,
-    width : "100%",
-    height: "100%",
-    borderRadius : 0,
-    delay : -0.2,
-    duration:0.6,
-    ease: "sine.inOut"
-  });
-  tl.to(".loaderDiv" , {
-    opacity:0,
-    delay : -0.3,
-    duration:0.9
-  });
-  tl.to(".circle2" , {
-    opacity:0,
-    delay : -0.2,
-    duration:0.3
-  });
-  
- 
-})
-
-// useGSAP(()=>{
- 
-//   tl.to(".circle" , {
-//     width : "100%",
-//     height: "100%",
-//     borderRadius : 0,
-//     delay : 1,
-//     duration:0.6
-//   });
-
-  
- 
-// })
-
-
 
   return (
-    <>
-      <div className="w-full h-screen relative overflow-hidden">
-      hero
-      <div className="w-4 h-4 bg-gray-950  rounded-full circle2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 opacity-0"></div>
-      <AnimatedLoadaer  imageRevel = {"assets/HeroSection/animatedLogo.png"}/>
-    </div>
+    <div className="h-screen px-10  flex justify-center items-center relative overflow-hidden">
+      <span className="MHstarting text-[6vmax] font-bold leading-[5.5rem] tracking-tighter absolute top-[20%] left-[5%] z-10">
+        <span className="text-primary">Taste</span> the Thrill{" "}
+      </span>
+      <span className="MHending text-[6vmax] font-bold leading-[5.5rem] tracking-tighter absolute top-[60%] right-[20%] z-10">
+        {" "}
+        of <span className="text-secondary">Now!</span>
+      </span>
+      <div className="w-[25%] h-[35vmax] rounded-lg  relative ">
+        <img
+          className="w-full h-full rounded-lg object-cover rotate-[6deg] origin-bottom-right absolute"
+          src={image2}
+          alt=""
+        />
+        <img
+          className="w-full h-full rounded-lg object-cover rotate-2 origin-bottom-right absolute"
+          src={image1}
+          alt=""
+        />
+        <img
+          className="w-full h-full rounded-lg object-cover -rotate-2 origin-bottom-right absolute"
+          src={image3}
+          alt=""
+        />
 
-
-
-    {/* <div className="w-full h-screen bg-white flex justify-center items-center relative">
-      <div className="w-4 h-4 bg-red-600 rounded-full circle absolute "></div>
-    </div> */}
-
-    
-
-
-
-{/* 
-    <div className="w-full h-screen bg-white flex justify-center items-center ">
-      <div className="w-[25vmax] h-[25vmax] bg-gradient-to-r from-primary to-secondary flex items-center justify-center overflow-hidden rounded-full circle">
-      <div className="w-fit h-fit font-bold text-[8vw] origin-center rotate-0 title">PEPSI</div>
+        {/* <video className='w-full h-full ' autoPlay muted controls>
+        <source src='./assets/HeroSecton/A-pepsi.mp4' type="video/mp4"/>
+        </video> */}
       </div>
-    </div> */}
-   
-    
-    </>
-  
+    </div>
+  );
+};
+// C:\Users\Prashant Hazariwal\Desktop\isro project\isro\public\assets\HeroSection\A-Pepsi.mp4
 
-
-  )
-}
-
-export default Hero
+export default Hero;
