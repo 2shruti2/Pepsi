fdf6248a031e087c93791ba53fa8a44067d01438
2shruti2 <shruthesharma@gmail.com>
2024-07-03 14:54:37 +0530

    bg changes

--- src/ui/sections/About.jsx
+++ src/ui/sections/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="relative">
     <ModelView />
-    <div className="max-sm:hidden absolute top-[61%] w-full bg-secondary h-64 overflow-hidden">
+    <div className="max-sm:hidden background absolute top-[61%] w-full bg-secondary h-64 overflow-hidden">
       <Glance />
     </div>
     <p className="max-sm:hidden w-full text-center fade-move-up text-3xl font-extrabold uppercase text-white italic absolute top-[61%] z-[100]">
